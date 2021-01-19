6dff0b1c4d717639570c91953020eefcebbe66a3
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-01-19 20:00:05 +0100

    Compare user number and mystery number

--- app/src/main/java/com/ynov/vernet/plusoumoins/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/plusoumoins/MainActivity.java
@@ -1,23 +1,23 @@
 package com.ynov.vernet.plusoumoins;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewBox;
     EditText editTextNumber;
 
+    int count = 0;
+
+    // Débug
     private static final String TAG = "MainActivity";
 
     @Override
@@ -25,11 +25,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int mysteryNumber = mysteryNumber();
 
-        Log.d(TAG, "onCreate: Mystery number : " + mysteryNumber());
+        Log.d(TAG, "onCreate: Mystery number : " + mysteryNumber);
 
-
+        textViewBox = findViewById(R.id.textViewBox);
         editTextNumber = findViewById(R.id.editTextNumber);
+
+        // Enter keyboard
+        editTextNumber.setOnKeyListener((v, keyCode, event) -> {
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                // Get user number from editText
+                String value = editTextNumber.getText().toString();
+                int userNumber = Integer.parseInt(value);
+
+                // Compare userNumber and mysterNumber
+                if (userNumber < mysteryNumber) {
+                    textViewBox.setText("The mystery number is bigger !");
+                    count++;
+                } else if (userNumber > mysteryNumber) {
+                    textViewBox.setText("The mystery number is lower !");
+                    count++;
+                } else if (userNumber == mysteryNumber) {
+                    textViewBox.setText("Good game ! ");
+                }
+                return true;
+            }
+            return false;
+        });
     }
 
     // Generate mystery number
