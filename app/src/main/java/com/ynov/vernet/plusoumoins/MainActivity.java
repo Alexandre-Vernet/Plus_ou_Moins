c0d4cc21bf1141087e4af69d3f63e400ae07d1de
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-01-19 22:57:30 +0100

    Add colors

--- app/src/main/java/com/ynov/vernet/plusoumoins/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/plusoumoins/MainActivity.java
@@ -1,8 +1,13 @@
 package com.ynov.vernet.plusoumoins;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,8 +17,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewBox;
+    TextView textViewInfo, textViewCount;
     EditText editTextNumber;
+    Button btnValidate;
 
     int count = 0;
 
@@ -25,30 +31,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int mysteryNumber = mysteryNumber();
-
-        Log.d(TAG, "onCreate: Mystery number : " + mysteryNumber);
-
-        textViewBox = findViewById(R.id.textViewBox);
+        textViewInfo = findViewById(R.id.textViewInfo);
+        textViewCount = findViewById(R.id.textViewCount);
         editTextNumber = findViewById(R.id.editTextNumber);
+        btnValidate = findViewById(R.id.btnValidate);
+
+        // Save mystery number
+        int mysteryNumber = mysteryNumber();
+        Log.d(TAG, "onCreate: " + mysteryNumber);
+
+        // Display count
+        textViewCount.setText(getString(R.string.counts, count));
 
         // Enter keyboard
         editTextNumber.setOnKeyListener((v, keyCode, event) -> {
             if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                 // Get user number from editText
                 String value = editTextNumber.getText().toString();
+
+                // Empty edit text
+                if (value.isEmpty()) {
+                    editTextNumber.setError(getString(R.string.textbox_empty));
+                    new Handler().postDelayed((Runnable) () -> editTextNumber.setError(null), 1000);
+                    return false;
+                }
+
+                // Convert value to int
                 int userNumber = Integer.parseInt(value);
 
-                // Compare userNumber and mysterNumber
-                if (userNumber < mysteryNumber) {
-                    textViewBox.setText("The mystery number is bigger !");
-                    count++;
-                } else if (userNumber > mysteryNumber) {
-                    textViewBox.setText("The mystery number is lower !");
-                    count++;
-                } else if (userNumber == mysteryNumber) {
-                    textViewBox.setText("Good game ! ");
+                // Compare userNumber and mysteryNumber
+                if (userNumber < mysteryNumber)
+                    textViewInfo.setText(R.string.number_bigger);
+                else if (userNumber > mysteryNumber)
+                    textViewInfo.setText(R.string.number_lower);
+
+
+                textViewInfo.setTextColor(getResources().getColor(R.color.red));
+                Vibrator vibe = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+                if (vibe != null) {
+                    vibe.vibrate(200);
                 }
+                count++;
+
+                /*Win*/
+                if (userNumber == mysteryNumber) {
+                    textViewInfo.setText(getString(R.string.win, count));
+                    textViewInfo.setTextColor(getResources().getColor(R.color.blue));
+                    editTextNumber.setVisibility(View.INVISIBLE);
+
+                    // Replay
+                    btnValidate.setText(R.string.replay);
+                    btnValidate.setOnClickListener(view -> {
+
+                    });
+                }
+
+                // Update count
+                textViewCount.setText(getString(R.string.counts, count));
+
+                // Open keyboard
+                editTextNumber.requestFocus();
+
                 return true;
             }
             return false;
@@ -62,6 +105,4 @@ public class MainActivity extends AppCompatActivity {
         Random r = new Random();
         return r.nextInt(max - min + 1) + min;
     }
-
-
 }
